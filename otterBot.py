3da01a020864b85326f7a1e90bb5fa0c9b67768c
Jan--Henrik <janhenrik@janhenrik.org>
2019-07-10 23:16:17 +0200

    added doku

--- otterBot.py
+++ otterBot.py
@@ -14,6 +14,7 @@ config.read('config.txt')
 botToken = config['DEFAULT']['botToken']
 update_id = None
 
+#function prototypes
 def otter(bot,update):
     _otter(bot,update)
 def mau(bot,update):
@@ -21,10 +22,15 @@ def mau(bot,update):
 def schleck(bot,update):
     _schleck(bot,update)
 
+#functions to be called + triggerwords
 fl =    [[otter,["Get-Image", "Whiteboard"]],
         [mau,["mau","Mau"]],
         [schleck,["schleck","Schleck","mlem","Mlem","vlem","Vlem"]]]
 
+#custom keyboard keys
+key =   [['Get-Image'],['Schleck']]
+
+#custom functions
 @restricted
 def _otter(bot,update):
     #Todo Webcam
@@ -43,9 +49,11 @@ def _schleck(bot,update):
 def _mau(bot,update):
     bot.send_message(update.message.chat_id, text="mau")
 
+
+###########
 @restricted
 def keyboard(bot,update):
-    reply_markup = telegram.ReplyKeyboardMarkup([['Get-Image'],['Schleck']])
+    reply_markup = telegram.ReplyKeyboardMarkup(key)
     bot.send_message(chat_id=update.message.chat_id,text="Chirp!",reply_markup=reply_markup)
 
 
